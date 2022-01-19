e3f1ec16dd6881bff3ec258b4f5fd2aced10d090
Manseerat Kaur <seeratsidhu01@gmail.com>
2022-01-19 16:27:16 -0500

    implemented vertical wordSearch using transpose, pair programmed with @Varsha

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,31 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const transpose = function(matrix) {
+let result = [];
+    for (let i = 0; i < matrix.length; i++){
+      for (let j = 0; j < matrix[i].length; j++){
+        if (result[j] === undefined) {
+            result[j] = [];
+        }
+        result[j][i] = matrix[i][j];
+      }
     }
-}
+    return result;
+};
+
+const checkHorizontal = (matrix, word) => {
+    const horizontalJoin = matrix.map(ls => ls.join(''));
+   
+    for (const l of horizontalJoin) {
+        if (l.includes(word)) return true;
+    }
+    return false;
+};
+
+const wordSearch = (letters, word) => { 
+   if (checkHorizontal(letters, word) || checkHorizontal(transpose(letters), word) && letters.length > 0) {
+       return true;
+   } 
+   return false;
+};
 
 module.exports = wordSearch
+
